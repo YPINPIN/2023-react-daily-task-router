8756947983c9b79517fff46a7d7275b02274cbcb
YPINPIN <yping611@gmail.com>
2026-02-14 18:57:02 +0800

    add navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import './App.css';
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
 
 const Home = () => {
   return <p>這是首頁</p>;
 };
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <Logout />
+    </>
+  );
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
 };
 const Register = () => {
   return <p>這是註冊頁面</p>;
+};
+
+const Logout = () => {
+  const navigate = useNavigate();
+  return <button onClick={() => navigate('/login')}>登出</button>;
 };
 
 const NotFound = () => {
