0964d9a9113451ca76ea34989e5977d62dcd3b1b
YPINPIN <yping611@gmail.com>
2026-02-14 18:45:27 +0800

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/2023-react-daily-task-router/' : '/',
   plugins: [react()],
-})
+}));
